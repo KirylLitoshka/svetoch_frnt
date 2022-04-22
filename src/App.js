7fd67b723d020d195d1e007cee40925182f35275
Kiryl Litoshka <kovzik08@gmail.com>
2022-04-22 10:40:57 +0300

    added main app view with test routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
+import "./styles/App.css";
+import Title from "./components/layout/title/Title";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Footer from "./components/ui/footer/Footer";
+import Header from "./components/ui/header/Header";
 
 function App() {
-  const message = ""
   return (
-    <div className="App">
-      <h3>{message}</h3>
+    <div className="page-wrapper">
+      <Router>
+        <Header/>
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Title/>}/>
+            {/*<Route path="/energy" element={<Energy/>}/>*/}
+            {/*<Route path="/water" element={<Water/>}/>*/}
+            {/*<Route path="/tbo" element={<TBO/>}/>*/}
+            {/*<Route path="/accounting" element={<Accounting/>}/>*/}
+          </Routes>
+        </main>
+        <Footer/>
+      </Router>
     </div>
   );
 }
