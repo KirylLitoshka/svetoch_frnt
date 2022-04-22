01cdd80526d961709508a6a37390efb890d0b08c
Kiryl Litoshka <kovzik08@gmail.com>
2022-04-22 10:37:53 +0300

    added base footer and styles

---
+++ src/components/ui/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__content">
+          <div>КЖУП "Светочь"</div>
+          <div>2022</div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
