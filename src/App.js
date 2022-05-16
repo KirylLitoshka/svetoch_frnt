86f145494ffe6b6a476c067ca3d6874c75a51da0
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-16 15:42:10 +0300

    updated main app with styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles/App.css";
-import Title from "./components/layout/title/Title";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Footer from "./components/ui/footer/Footer";
-import Header from "./components/ui/header/Header";
+import AppRouter from "./routes/main/AppRouter";
+
 
 function App() {
+  const [currentSystem, setCurrentSystem] = useState({})
+
   return (
-    <div className="page-wrapper">
-      <Router>
-        <Header/>
-        <main className="main">
-          <Routes>
-            <Route path="/" element={<Title/>}/>
-            {/*<Route path="/energy" element={<Energy/>}/>*/}
-            {/*<Route path="/water" element={<Water/>}/>*/}
-            {/*<Route path="/tbo" element={<TBO/>}/>*/}
-            {/*<Route path="/accounting" element={<Accounting/>}/>*/}
-          </Routes>
-        </main>
-        <Footer/>
-      </Router>
-    </div>
+      <div className="page-wrapper">
+        <AppRouter currentApp={currentSystem} changeCurrentApp={setCurrentSystem} />
+      </div>
   );
 }
 
