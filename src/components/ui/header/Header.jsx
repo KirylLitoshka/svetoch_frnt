a5a64aeca89e8009b0c71c90487f5aa6ef3c75bb
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-16 15:39:52 +0300

    updated header padding and added current subsystem into header

--- src/components/ui/header/Header.jsx
+++ src/components/ui/header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import "./Header.css"
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className="header">
       <div className="container">
         <div className="header__content">
           <a href="/">На главную</a>
         </div>
+        {props.currentApp &&
+            <div>
+              {props.currentApp.alt_name}
+            </div>
+        }
       </div>
     </header>
   );
