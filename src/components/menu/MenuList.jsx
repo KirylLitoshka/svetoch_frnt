fec54ac82354bde8d337f3430f48ce1e52d87eb5
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-23 15:32:02 +0300

    apply vscode linter

--- src/components/menu/MenuList.jsx
+++ src/components/menu/MenuList.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import MenuItem from "./MenuItem";
 import "./MenuList.css";
 
-const MenuList = ({items}) => {
+const MenuList = ({ items }) => {
   return (
-      <div className="menu-list">
-        {items.map(item =>
-          <MenuItem key={item.id} item={item}/>
-        )}
-      </div>
+    <div className="menu-list">
+      {items.map((item) => (
+        <MenuItem key={item.id} item={item} />
+      ))}
+    </div>
   );
 };
 
