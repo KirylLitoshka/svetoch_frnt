81de9c05169569342adcec91112499d557d194f8
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-23 15:29:55 +0300

    fixed link path

--- src/components/menu/MenuItem.jsx
+++ src/components/menu/MenuItem.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import "./MenuItem.css"
-import {Link} from "react-router-dom";
+import React from "react";
+import "./MenuItem.css";
+import { Link } from "react-router-dom";
 
-const MenuItem = ({item}) => {
+const MenuItem = ({ item }) => {
   return (
-      <div className="menu-list__item">
-        <Link to={"/" + item.name} className="menu-list__link">
-          {item.alt_name}
-        </Link>
-      </div>
+    <div className="menu-list__item">
+      <Link to={`${item.name}`} className="menu-list__link">
+        {item.alt_name}
+      </Link>
+    </div>
   );
 };
 
