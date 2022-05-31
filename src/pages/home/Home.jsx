53331cc1301262d002cf5a0260565600ab637b7c
Kirill Litoshko <kovzik08@gmail.com>
2022-05-31 12:04:01 +0300

    replaced useState hook with static data (will be replaced back)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import MenuList from "../../components/menu/MenuList";
 import "./Home.css"
 
 const Home = () => {
-    const [services, setServices] = useState([
+    const services = [
         {id: 1, name: "renters", alt_name: "Арендаторы"}
-    ])
+    ]
 
     return (
         <div className="menu">
