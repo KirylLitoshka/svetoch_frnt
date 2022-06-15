15828381d5e96b59be71a43b57f43f0fa718e99c
Kirill Litoshko <kovzik08@gmail.com>
2022-06-15 16:24:02 +0300

    added test routing (with component)

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useMatch, useParams } from 'react-router-dom';
+import {useLocation, useMatch, useParams} from 'react-router-dom';
 
-const Test = () => {
-  const params = useParams()
-  const temp = useMatch("")
+const Test = (props) => {
+    const params = useParams()
+    const temp = useMatch("")
+    const location = useLocation()
+    console.log(location)
 
-  return (
-      <div>
-        Some test value
-      </div>
-  );
+
+    return (
+        <div>
+            Some test value
+        </div>
+    );
 };
 
 export default Test;
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,13 +3,15 @@ import Error from "../components/ui/error/Error";
 import Home from "../pages/home/Home";
 import {Route, Routes } from "react-router-dom";
 import Renters from "../pages/services/renters/Renters";
+import Test from "../pages/Test";
 
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="renters/" element={<Renters />} />
+      <Route path="renters" element={<Renters />}/>
+      <Route path="renters/asd" element={<Test/>}/>
       <Route path="*" element={<Error message="404" />} />
     </Routes>
   );
