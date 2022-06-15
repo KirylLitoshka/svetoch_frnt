c22fa41f3e25fd82fe5f9a6aad6a2ca7f2f89c45
Kirill Litoshko <kovzik08@gmail.com>
2022-06-15 16:24:53 +0300

    removed list header (will be replaced by static value)

--- src/components/renters/RentersList.jsx
+++ src/components/renters/RentersList.jsx
@@ -2,18 +2,10 @@ import React from 'react';
 import RenterItem from "./RenterItem";
 import "./RentersList.css";
 
-const RentersList = ({items}) => {
-    const listHeader = {
-        name: "Наименование",
-        agreement: "Договор",
-        legal_address: "Адрес",
-        mail_address: "Почтовый адрес",
-        phone: "Телефон"
-    }
+const RentersList = ({items, modalVisible, setItemID}) => {
     return (
         <div className="renters-list">
-            <RenterItem item={listHeader}/>
-            {items.map(item => <RenterItem key={item.id} item={item}/>)}
+            {items.map(item => <RenterItem key={item.id} item={item} modalVisible={modalVisible} setItemID={setItemID}/>)}
         </div>
     );
 };
